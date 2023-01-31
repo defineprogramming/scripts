c5a1989ac8db8ca2883d1bd2d58eb4c6866ac5f2
Leo Cooper <113992401+defineprogramming@users.noreply.github.com>
2023-01-31 13:45:27 +0000

    Update thing.bat

--- thing.bat
+++ thing.bat
@@ -1,4 +1,7 @@
-REM Write a batch script that uninstalls AB Tutor
+REM Write a Batch code that disables policies blocking the use of the Remote Shutdown dialogue.
 
-REM Delete the AB Tutor folder
-rmdir /s /q "C:\Program Files\AB Tutor"
+REM Disable the policy that blocks the use of the Remote Shutdown dialogue.
+REG ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableCAD /t REG_DWORD /d 0 /f
+
+REM Open the Remote Shutdown dialogue.
+shutdown -i

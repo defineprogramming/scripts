f41fa7cfefffaae7c007be646cd6597b666359f8
Leo Cooper <113992401+defineprogramming@users.noreply.github.com>
2023-01-31 13:41:00 +0000

    Update thing.bat

--- thing.bat
+++ thing.bat
@@ -1,14 +1,4 @@
-REM Write a batch script that installs the latest version of Python (3.11) and cirumvents any permission issues:
+REM Write a batch script that uninstalls AB Tutor
 
-@echo off
-setlocal
-set "py=python-3.11.0-amd64.exe"
-set "pyurl=https://www.python.org/ftp/python/3.11.0/%py%"
-set "pydir=C:\Python311"
-set "pyexe=%pydir%\python.exe"
-set "pyget=%temp%\%py%"
-
-echo Downloading Python...
-powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%pyurl%', '%pyget%')"
-
-
+REM Delete the AB Tutor folder
+rmdir /s /q "C:\Program Files\AB Tutor"

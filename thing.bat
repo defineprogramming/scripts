c92e9966c9cbcd8880a4630e8fb0bce248e39f45
Leo Cooper <113992401+defineprogramming@users.noreply.github.com>
2023-01-31 13:46:20 +0000

    Update thing.bat

--- thing.bat
+++ thing.bat
@@ -1,7 +1,1 @@
-REM Write a Batch code that disables policies blocking the use of the Remote Shutdown dialogue.
-
-REM Disable the policy that blocks the use of the Remote Shutdown dialogue.
-REG ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableCAD /t REG_DWORD /d 0 /f
-
-REM Open the Remote Shutdown dialogue.
 shutdown -i

8e08d178200efb7eb0016afdc9df0612b8ae9899
Leo Cooper <113992401+defineprogramming@users.noreply.github.com>
2023-01-31 13:50:43 +0000

    Update thing.bat

--- thing.bat
+++ thing.bat
@@ -1,1 +1,4 @@
-curl -O https://www.python.org/ftp/python/3.11.0/python-3.11.0-amd64.exe
+REM Write a Batch script that disables the policy to block Chrome settings.
+@echo off
+REM Disable the policy to block Chrome settings.
+REG ADD "HKLM\SOFTWARE\Policies\Google\Chrome" /v ExtensionInstallForcelist /t REG_SZ /d "" /f

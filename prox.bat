e4525f11aa3ef1e2fb30bcded2b166508fad0862
Leo Cooper <113992401+defineprogramming@users.noreply.github.com>
2023-03-22 13:45:33 +0000

    Update prox.bat

--- prox.bat
+++ prox.bat
@@ -1,2 +1,5 @@
-@ECHO OFF
+@echo off
 REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 0 /f
+powershell -Command "& {$dll = '[DllImport(\"wininet.dll\", SetLastError = true)] public static extern bool InternetSetOption(IntPtr hInternet, int dwOption, IntPtr lpBuffer, int dwBufferLength); public static bool Refresh() { return InternetSetOption(IntPtr.Zero, 39, IntPtr.Zero, 0) && InternetSetOption(IntPtr.Zero, 37, IntPtr.Zero, 0); }'; add-type -MemberDefinition $dll -name wininet -namespace pinvoke; [pinvoke.wininet]::Refresh()}"
+echo Proxy disabled.
+pause

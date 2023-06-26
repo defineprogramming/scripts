612d279020258988d0d23fb066499682d12567e4
Leo Cooper <113992401+defineprogramming@users.noreply.github.com>
2023-06-26 09:11:54 +0100

    Update proxnew.bat

--- proxnew.bat
+++ proxnew.bat
@@ -1,11 +1,9 @@
 @echo off
 :: This batch file disables the proxy settings for the current user
 
-:: Disable the proxy for the current user
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 0 /f
-
-:: Clear the proxy address for the current user
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /t REG_SZ /d "" /f
+:: Launch PowerShell to disable the proxy
+powershell -Command "Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' -name ProxyEnable -value 0"
+powershell -Command "Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' -name ProxyServer -value ''"
 
 echo Proxy settings for the current user have been disabled.
 pause
